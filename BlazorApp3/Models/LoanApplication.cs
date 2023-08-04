0af5ac8acaefd6687a53aefb361a1451013ede7b
WiJungle <WiJungle@LAPTOP-9DO6IV1I>
2023-08-04 10:43:29 +0530

    Add project files.

---
+++ BlazorApp3/Models/LoanApplication.cs
@@ -0,0 +1,26 @@
+﻿
+
+namespace BlazorApp3.Models
+{
+	[Serializable]
+	public class LoanApplication
+	{
+		public string FullName { get; set; }
+		public string FatherName { get; set; } 
+		public string Email { get; set; }
+        public string BranchCode { get; set; }
+
+		public string PhoneNo { get; set; }
+
+		public string AadharNo { get; set; }
+
+		public int LoanAmount { get; set; }
+
+		public string Address { get; set;}
+
+		public string City { get; set;}
+
+		public string Pincode { get; set;}
+
+    }
+}
